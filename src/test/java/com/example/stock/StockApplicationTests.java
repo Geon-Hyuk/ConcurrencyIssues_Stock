f568d5c52b973bf3aa68c635369611d29fa591e8
igh_9410 <31177070+Geon-Hyuk@users.noreply.github.com>
2022-09-25 09:37:42 +0900

    Initial commit

---
+++ src/test/java/com/example/stock/StockApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.stock;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import com.example.stock.service.PessimisticLockStockService;
+import com.example.stock.service.StockService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
