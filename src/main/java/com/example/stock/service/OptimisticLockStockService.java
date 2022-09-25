933cdf0b6e93f26ae6c2f6cceafa0e451bc1172f
igh_9410 <31177070+Geon-Hyuk@users.noreply.github.com>
2022-09-25 15:36:52 +0900

    Database Locks

--- src/main/java/com/example/stock/service/OptimisticLockStockService.java
+++ src/main/java/com/example/stock/service/OptimisticLockStockService.java
@@ -3,30 +3,23 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
-public class StockService {
+public class OptimisticLockStockService {
 
     private StockRepository stockRepository;
 
-    public StockService(StockRepository stockRepository) {
+    public OptimisticLockStockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public synchronized void decrease(Long id, Long quantity) {
-        // get stock
-        // 재고감소
-        // 저장
-
-        Stock stock = stockRepository.findById(id).orElseThrow();
-
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithOptimisticLock(id);
         stock.decrease(quantity);
 
         stockRepository.saveAndFlush(stock);
-
     }
 }
